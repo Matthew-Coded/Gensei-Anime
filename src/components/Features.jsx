370dbebc7fb02cf457602ba1f4e3558e83d2a575
Matthew-Coded <7matthew.bautista7@gmail.com>
2026-02-16 15:52:48 -0800

    hero page done

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const Features = () => {
+
+  const features = [
+    {
+      title: 'Popular',
+      description: "Watch from a list of highly rated anime where any you chose you'll bound to be hooked!",
+      
+    }
+  ]
+
   return (
     <div>Features</div>
   )
---
+++ src/components/FloatingCards.js
@@ -0,0 +1,31 @@
+export const floatingCards = {
+    'Popular': {
+        bgColor: 'bg-blue-500/20',
+        iconColor: 'text-blue-400',
+        textColor: 'text-blue-200',
+        contentColor: 'text-blue-300',
+        icon: 'GA',
+        title: 'Highly Rated',
+        content: 'Watch the most popular anime here!'
+    },
+
+    'New': {
+        bgColor: 'bg-blue-500/20',
+        iconColor: 'text-pink-400',
+        textColor: 'text-pink-200',
+        contentColor: 'text-pink-300',
+        icon: 'GA',
+        title: 'New Episodes',
+        content: 'Is it the day your show comes out?'
+    },
+
+    'This Season': {
+        bgColor: 'bg-blue-500/20',
+        iconColor: 'text-orange-400',
+        textColor: 'text-orange-200',
+        contentColor: 'text-orange-300',
+        icon: 'GA',
+        title: 'Spring, Summer, Fall, Winter...',
+        content: 'New seasons! Newly released Anime!'
+    },
+}
